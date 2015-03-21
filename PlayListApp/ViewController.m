a4d096d596dc336d9a9a3f8dbae3dffa4f15cac9
Harish <harish.upad@gmail.com>
2015-03-21 10:21:14 -0500

    Initial Commit

---
+++ PlayListApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PlayListApp
+//
+//  Created by Harish Upadhyayula on 3/21/15.
+//  Copyright (c) 2015 Harish Upadhyayula. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ PlayListApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlayListApp
+//
+//  Created by Harish Upadhyayula on 3/21/15.
+//  Copyright (c) 2015 Harish Upadhyayula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
